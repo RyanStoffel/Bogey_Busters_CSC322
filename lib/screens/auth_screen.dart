e594f18dbcccafdb10df0cf43ceb0f575c4a6744
RyanStoffel <772612@calbaptist.edu>
2025-11-05 12:53:55 -0800

    Remove unused Google Logo import from auth screen.

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import '../services/auth_service.dart';
 import 'verify_email_screen.dart';
 import 'home_screen.dart';
